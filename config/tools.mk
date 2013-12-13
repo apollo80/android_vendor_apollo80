856fefa41c59f0048a114d1e54b0a7f93688bfad
apollo <apollo80@list.ru>
2013-12-13 12:15:58 +0400

    add support extapps

---
+++ config/tools.mk
@@ -0,0 +1,5 @@
+
+# Utilize init.d scripts
+PRODUCT_COPY_FILES += \
+    vendor/apollo80/prebuild/common/etc/init.d/05mountext:system/etc/init.d/05mountext \
+    vendor/apollo80/prebuild/common/etc/init.d/20extapps:system/etc/init.d/20extapps
