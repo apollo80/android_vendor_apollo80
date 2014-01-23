31476e9b352ee92ae0d1276c6e730964e04563c5
apollo <apollo80@list.ru>
2014-01-23 21:43:08 +0400

    update extapps script:   - added logging   - added mounting /sd-ext   - added function to create links with sd-ext on system   - added bind mounting remove old mountext script

--- config/tools.mk
+++ config/tools.mk
@@ -1,5 +1,10 @@
 
 # Utilize init.d scripts
 PRODUCT_COPY_FILES += \
-    vendor/apollo80/prebuild/common/etc/init.d/05mountext:system/etc/init.d/05mountext \
-    vendor/apollo80/prebuild/common/etc/init.d/20extapps:system/etc/init.d/20extapps
+    vendor/apollo80/prebuild/common/etc/init.d/05extapps:system/etc/init.d/05extapps \
+    vendor/apollo80/prebuild/common/etc/extapps/default_paths:system/etc/extapps/default_path \
+    vendor/apollo80/prebuild/common/etc/extapps/default_params:system/etc/extapps/default_params \
+    vendor/apollo80/prebuild/common/etc/extapps/log:system/etc/extapps/log \
+    vendor/apollo80/prebuild/common/etc/extapps/makelink2system:system/etc/extapps/makelink2system \
+    vendor/apollo80/prebuild/common/etc/extapps/mount_bind:system/etc/extapps/mount_bind \
+    vendor/apollo80/prebuild/common/etc/extapps/mount_sdext:system/etc/extapps/mount_sdext
