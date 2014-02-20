2f5379d104724d3fbd53518f12964e9a86c07830
apollo80 <apollo80@list.ru>
2014-02-20 13:54:18 +0400

    added bravo device and rename files
    Change-Id: Ib1e6eaa22aa1be5837649fbc27565b8aefce29b0

--- config/tools.mk
+++ config/tools.mk
@@ -2,8 +2,8 @@
 # Utilize init.d scripts
 PRODUCT_COPY_FILES += \
     vendor/apollo80/prebuild/common/etc/init.d/05extapps:system/etc/init.d/05extapps \
-    vendor/apollo80/prebuild/common/etc/extapps/default_paths:system/etc/extapps/default_path \
-    vendor/apollo80/prebuild/common/etc/extapps/default_params:system/etc/extapps/default_params \
+    vendor/apollo80/prebuild/common/etc/extapps/enviroment:system/etc/extapps/enviroment \
+    vendor/apollo80/prebuild/common/etc/extapps/settings:system/etc/extapps/settings \
     vendor/apollo80/prebuild/common/etc/extapps/log:system/etc/extapps/log \
     vendor/apollo80/prebuild/common/etc/extapps/makelink2system:system/etc/extapps/makelink2system \
     vendor/apollo80/prebuild/common/etc/extapps/mount_bind:system/etc/extapps/mount_bind \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,2 @@
 add_lunch_combo cm_passion-userdebug
+add_lunch_combo cm_bravo-userdebug
