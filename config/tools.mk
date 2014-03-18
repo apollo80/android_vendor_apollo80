ae00f6a332cc87ac2bc57c3d5557c850d1341726
apollo80 <apollo80@list.ru>
2014-03-18 14:18:13 +0400

    updated extapps scripts
    Change-Id: I5b99f414e27f4bd0334a7305d5d3dd72cc913e27

--- config/tools.mk
+++ config/tools.mk
@@ -1,8 +1,8 @@
 
 # Utilize init.d scripts
 PRODUCT_COPY_FILES += \
+    vendor/apollo80/prebuild/common/bin/extapps:system/bin/extapps \
     vendor/apollo80/prebuild/common/etc/init.d/05extapps:system/etc/init.d/05extapps \
-    vendor/apollo80/prebuild/common/etc/extapps/enviroment:system/etc/extapps/enviroment \
     vendor/apollo80/prebuild/common/etc/extapps/settings:system/etc/extapps/settings \
     vendor/apollo80/prebuild/common/etc/extapps/log:system/etc/extapps/log \
     vendor/apollo80/prebuild/common/etc/extapps/makelink2system:system/etc/extapps/makelink2system \
